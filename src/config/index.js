5526cff6cca2d2c05e342fb62d1e337693b9e4d9
Anniez94 <c.mokwunye@taxaide.com.ng>
2021-10-01 22:24:59 +0100

    test heroku database

--- src/config/index.js
+++ src/config/index.js
@@ -2,28 +2,37 @@ const Sequelize = require("sequelize");
 const config = require("./config");
 require("dotenv/config");
 
-let dev_db = new Sequelize(config.development.database, config.development.username, config.development.password, {
-  dialect: config.development.dialect,
-  host:config.development.host,
-  logging: false, 
-  define: {
-      freezeTableName: true
-    }
-});
+let dev_db = new Sequelize(
+  config.development.database,
+  config.development.username,
+  config.development.password,
+  {
+    dialect: config.development.dialect,
+    host: config.development.host,
+    logging: false,
+    define: {
+      freezeTableName: true,
+    },
+  }
+);
 
-let prod_db  = new Sequelize(process.env.DATABASE_URL, {
-    dialect: 'postgres',
-    dialectOptions: {
-      ssl: {
-        require: true,
-        rejectUnauthorized: false 
-      }
-    }
-})
+let prod_db = new Sequelize(process.env.DATABASE_URL, {
+  dialect: "postgres",
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+  },
+  logging: false,
+  define: {
+    freezeTableName: true,
+  },
+});
 
 // new Sequelize(config.production.database, config.production.username, config.production.password, {
 //   dialect: config.production.dialect,
-//   ssl: true, 
+//   ssl: true,
 //   dialectOptions: {
 //     ssl: {
 //       require: true, // This will help you. But you will see nwe error
@@ -32,13 +41,11 @@ let prod_db  = new Sequelize(process.env.DATABASE_URL, {
 //   },
 //   host:config.production.host,
 //   port:config.production.port,
-//   logging: false, 
+//   logging: false,
 //   define: {
 //       freezeTableName: true
 //     }
 // });
 // new Sequelize(process.env.DATABASE_URL)
 
-
-
-module.exports =  process.env.NODE_ENV === "development" ? dev_db : prod_db
+module.exports = process.env.NODE_ENV === "development" ? dev_db : prod_db;
