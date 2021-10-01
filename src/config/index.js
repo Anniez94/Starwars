0c9a99cd1ba4c01416c76f04ead3e2e2ae2e599d
Anniez94 <c.mokwunye@taxaide.com.ng>
2021-10-01 21:47:02 +0100

    test heroku database

--- src/config/index.js
+++ src/config/index.js
@@ -12,14 +12,17 @@ let dev_db = new Sequelize(config.development.database, config.development.usern
     }
 });
 
-let prod_db = new Sequelize(config.production.database, config.production.username, config.production.password, {
-  dialect: config.production.dialect,
-  host:config.production.host,
-  port:config.production.port,
-  logging: false, 
-  define: {
-      freezeTableName: true
-    }
-});
+let prod_db  = new Sequelize(process.env.DATABASE_URL)
+// new Sequelize(`postgres://${config.production.username}:${config.production.password}@${config.production.host}/${config.production.database}`)
+
+// new Sequelize(config.production.database, config.production.username, config.production.password, {
+//   dialect: config.production.dialect,
+//   host:config.production.host,
+//   port:config.production.port,
+//   logging: false, 
+//   define: {
+//       freezeTableName: true
+//     }
+// });
 
 module.exports = process.env.NODE_ENV == "development" ? dev_db : prod_db
