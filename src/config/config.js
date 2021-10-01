f90011ded039b07abba49af64cf0978db7712dfd
Anniez94 <c.mokwunye@taxaide.com.ng>
2021-10-01 19:46:53 +0100

    convert mysql to heroku postgres sql

--- src/config/config.js
+++ src/config/config.js
@@ -6,13 +6,13 @@ module.exports = {
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE,
     host: process.env.DB_HOST,
-    dialect: "mysql"
+    dialect: "postgres"
   },
     production: {
     username: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE,
     host: process.env.DB_HOST,
-    dialect: "mysql"  
+    dialect: "postgres" 
   }
 };
