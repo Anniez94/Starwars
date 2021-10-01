cace969e53f898168662e7714d31c9dd98d4b222
Anniez94 <c.mokwunye@taxaide.com.ng>
2021-10-01 22:31:14 +0100

    test heroku database

--- src/config/index.js
+++ src/config/index.js
@@ -30,22 +30,4 @@ let prod_db = new Sequelize(process.env.DATABASE_URL, {
   },
 });
 
-// new Sequelize(config.production.database, config.production.username, config.production.password, {
-//   dialect: config.production.dialect,
-//   ssl: true,
-//   dialectOptions: {
-//     ssl: {
-//       require: true, // This will help you. But you will see nwe error
-//       rejectUnauthorized: false // This line will fix new error
-//     }
-//   },
-//   host:config.production.host,
-//   port:config.production.port,
-//   logging: false,
-//   define: {
-//       freezeTableName: true
-//     }
-// });
-// new Sequelize(process.env.DATABASE_URL)
-
 module.exports = process.env.NODE_ENV === "development" ? dev_db : prod_db;
